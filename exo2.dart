20a611fc3a45c9f2bc81307a500e20cec720ade9
bekkai-mohibb <koukou0719@hotmail.com>
2025-11-11 11:17:41 -0500

    exo2_qst123

---
+++ exo2.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main() {
+  try {
+   
+    stdout.write("Entrez votre age : ");
+    String? saisie = stdin.readLineSync();
+
+    
+    int age = int.parse(saisie!);
+    print("Votre age est : $age ans");
+
+  } on FormatException catch (e) {
+    print("Erreur : le format saisi n'est pas valide ");
+  } 
+}
